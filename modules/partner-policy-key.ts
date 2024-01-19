85b4a4c27713b797f8fcd4ccea979598bd87b591
pawlek <pavlo.voronyuk@gmail.com>
2024-01-19 14:32:27 +0000

    check partner status

--- modules/partner-policy-key.ts
+++ modules/partner-policy-key.ts
@@ -1,16 +1,24 @@
-import { ZuploContext, ZuploRequest } from "@zuplo/runtime";
+import { ZuploContext, ZuploRequest, HttpProblems } from "@zuplo/runtime";
 
 type MyPolicyOptionsType = {
   myOption: any;
 };
+
 export default async function (
   request: ZuploRequest,
   context: ZuploContext,
   options: MyPolicyOptionsType,
   policyName: string
 ) {
-  request.headers.set('authorization', 'Bearer ' + request.user.data['key']);
-  request.headers.set('partnername', request.user.data['name']);
-
+  if (request.user?.data.status === "active") {   
+    request.headers.set('authorization', 'Bearer ' + request.user.data['key']);
+    request.headers.set('partnername', request.user.data['name']);
+  } else {
+    // Return the bad request response immediately if the condition is not met
+    return HttpProblems.badRequest(request, context, {
+      detail: "The consumer status is not active.",
+    });
+  }
+  
   return request;
 }
